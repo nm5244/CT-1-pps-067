2b4c96d494bae2fea732fcdeff2f58283b0c8036
nm5244 <102832058+nm5244@users.noreply.github.com>
2022-04-26 22:48:08 +0530

    Add files via upload

---
+++ ct1pps2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+  char op;
+  float first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%f %f", &first, &second);
+
+  switch (op) {
+    case '+':
+      printf("%f + %f = %f", first, second, first + second);
+      break;
+    case '-':
+      printf("%f - %f = %f", first, second, first - second);
+      break;
+    case '*':
+      printf("%f * %f = %f", first, second, first * second);
+      break;
+    case '/':
+      printf("%f / %f = %f", first, second, first / second);
+      break;
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
+
+  return 0;
+}
